4167d931eff60d459cd5a03a26a1642eaff50b74
Spencer Michalski <semski13@gmail.com>
2022-09-16 17:30:35 -0400

    add inputs for metrics

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input data-testid="input-monthly-payment" />
+      <input data-testid="input-monthly-rent" />
+      <input data-testid="input-reserves" />
+      <input data-testid="input-net-worth-roi" />
+    </>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders number inputs", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  expect(screen.getByTestId("input-monthly-payment")).toBeInTheDocument();
+  expect(screen.getByTestId("input-monthly-rent")).toBeInTheDocument();
+  expect(screen.getByTestId("input-reserves")).toBeInTheDocument();
+  expect(screen.getByTestId("input-net-worth-roi")).toBeInTheDocument();
 });
